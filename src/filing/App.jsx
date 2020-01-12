f2d0dc0fbde0b4636c9870ccdabbbad61a6fe4c8
meissadia <meissadia@gmail.com>
2020-01-12 15:16:18 -0700

    Validate quarterly filingPeriods

--- src/filing/App.jsx
+++ src/filing/App.jsx
@@ -12,7 +12,7 @@ import isRedirecting from './actions/isRedirecting.js'
 import updateFilingPeriod from './actions/updateFilingPeriod.js'
 import { detect } from 'detect-browser'
 import { splitYearQuarter } from './api/utils.js'
-import { currentQuarterlyPeriod } from './constants/dates'
+import { currentQPeriod, isValidQPeriod } from './utils/dateQuarterly.js'
 
 import 'normalize.css'
 import './app.css'
@@ -73,10 +73,12 @@ export class AppContainer extends Component {
   }
 
   isValidPeriod(period) {
-    const [year] = splitYearQuarter(period)
     const filingPeriods = this.props.config.filingPeriods
-
-    return filingPeriods.indexOf(year) !== -1 || period === currentQuarterlyPeriod()
+    const [year, quarter] = splitYearQuarter(period)
+    const currQYear = splitYearQuarter(currentQPeriod())[0]
+    
+    if(quarter) return isValidQPeriod(period, filingPeriods) 
+    return filingPeriods.indexOf(year) !== -1 || period === currQYear
   }
 
   render() {
